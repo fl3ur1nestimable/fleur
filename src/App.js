85bcae6a8d34913a9d49273e2a59aba3f58bf0c6
Antoine.Yebouet <antoine.yebouet@telecomnancy.eu>
2023-06-21 00:20:21 +0200

    responsive split cool

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ function App() {
   return (
     <>
       <div className="split left">
+        <div className="fleur">
+          <h1>Fl3ur_1nestimable</h1>
+          <h2>Music | Sports | Socials</h2>
+          <button className="btn_left">Let's go</button>
+        </div>
         <img src="arrivalyellow.png" alt="arrivalyellow" className='imgbanner'/>
       </div>
       <div className= "split right">
+        <div className="ayeb">
+          <h1>Antoine Yebouet</h1>
+          <h2>Portfolio | Resume | Projects</h2>
+          <button className="btn_right">Enter</button>
+        </div>
         <img src="arrivalrose.png" alt="arrivalrose" className='imgbanner' />
       </div>
     </>
