fd18499f0f8d471a24a84532f367fc702e023c79
Antoine.Yebouet <antoine.yebouet@telecomnancy.eu>
2023-06-21 15:20:35 +0200

    phone welcome

--- src/App.js
+++ src/App.js
@@ -14,16 +14,24 @@ function App() {
     <>
       {
         (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ? (
-          <View style={{height:'100%',border:'3px solid red'}}>
-            <View style={styles.splitTop}>
-                <View style={styles.fleurM}>
-                  <Text style={styles.title} >Fl3ur_1nestimable</Text>
-                  <Text style={styles.subtitle}>Music | Sports | Socials</Text>
-                  <Button style={styles.btntop} title="Let's go" />
-                </View>
-            </View>
-      
-          </View>
+          <>
+          <div className="splitM top">
+            <div className="fleurM">
+              <h1>Fl3ur_1nestimable</h1>
+              <h2>Music | Sports | Socials</h2>
+              <button className="mbtn_left">Let's go</button>
+            </div>
+            <img src="arrivalyellowfull.png" alt="arrivalyellow" className='imgbanner' />
+          </div>
+          <div className="splitM bot">
+          <div className="ayebM">
+            <h1>Antoine Yebouet</h1>
+            <h2>Music | Sports | Socials</h2>
+            <button className="mbtn_left">Let's go</button>
+          </div>
+          <img src="arrivalrosefull.png" alt="arrivalyellow" className='imgbanner' />
+        </div>
+        </>
         ) : (
           <>
             <div className="split left">
